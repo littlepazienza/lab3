4b3619ee537173e9e917d1ca53f778ec6dc111c0
Nick <ntownsley@linus.cs.loyola.edu>
2018-09-18 13:23:37 -0400

    RightTriangle

---
+++ RightTriangle.java
@@ -0,0 +1,33 @@
+
+public class RightTriangle extends Shape 
+{
+	
+	double length, width;
+
+	public RightTriangle(Color c, String n, double in_length, double in_width) 
+	{
+		super(c, n);
+		length = in_length;
+		width = in_width;
+		
+		
+	}
+
+	@Override
+	public double area() 
+	{
+		
+		return (0.5*length*width);
+	}
+
+	@Override
+	public double perimeter() 
+	{
+		
+		return Math.sqrt((length*length)+(width*width));
+	}
+	
+	
+	
+
+}
--- Tester.java
+++ Tester.java
@@ -5,5 +5,12 @@ public class Tester
     // Example test case
     // Star s = new Star(Shape.Color.green, 5);
     // System.out.println("star = " + s);
+	  
+	//Test for RightTriangle
+	RightTriangle rt = new RightTriangle(Shape.Color.red, "triangle", 4, 4);
+	System.out.println("Test for Right Triangle (should be A red triangle area 8.0 perimeter 5.656854249492381)="+rt);
+	
+	RightTriangle rt2 = new RightTriangle(Shape.Color.green, "triangle2", 12, 20);
+	System.out.println("Test for Right Triangle (should be a green triangle2 area 120.0 perimeter 23.323807579381203)=" + rt2);
   }
 }
